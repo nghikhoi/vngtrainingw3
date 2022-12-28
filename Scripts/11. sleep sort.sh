4d4a544b937dfa590b9c569ddad8891b45dbb1eb
Nghi Khôi <nghikhoi1108@gmail.com>
2022-12-29 04:24:26 +0700

    Fix sleep sort scripts

--- Scripts/11. sleep sort.sh
+++ Scripts/11. sleep sort.sh
@@ -1,16 +1,20 @@
-#!/usr/bin/zsh
+#!/usr/bin/zsh -m
+setopt monitorC
+
 tmp=$(mktemp)
 
 argc=${#@[@]}
 
 for i in $@; do
-  printf "sleep %de-2; echo %d \n" $pid $i $i >> $tmp
+  printf "sleep %de-1; echo %d\n" $i $i >> $tmp
 done
 
+cat $tmp
 {
 cat $tmp | xargs -P $argc -I {} sh -c 'eval "$1"' - {}
 } &
 
+pid=$!
 
 # Script execution continues here while `xargs` is running
 # in the background.
@@ -18,7 +22,7 @@ echo "Waiting for commands to finish..."
 
 # Wait for `xargs` to finish, via special variable $!, which contains
 # the PID of the most recently started background process.
-wait $!
+wait
 
 
 rm -f $tmp
